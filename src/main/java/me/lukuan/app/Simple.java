8447e1156b697ced66bd6021f86709edf341238e
Kuan Lu <czlukuan@gmail.com>
2021-03-26 20:00:41 +0800

    add demo file

---
+++ src/main/java/me/lukuan/app/Simple.java
@@ -0,0 +1,13 @@
+package me.lukuan.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Simple
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
